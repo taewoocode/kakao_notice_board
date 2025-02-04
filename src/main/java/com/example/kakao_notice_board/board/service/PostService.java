f1058a3a37751967d5bb2c31aa98df301c46320a
taewoocode <qkrxodn6035@gmail.com>
2025-02-05 03:19:00 +0900

    포스트 서비스 구현

---
+++ src/main/java/com/example/kakao_notice_board/board/service/PostService.java
@@ -0,0 +1,51 @@
+package com.example.kakao_notice_board.board.service;
+
+import com.example.kakao_notice_board.board.domain.Post;
+import com.example.kakao_notice_board.board.repository.PostRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class PostService {
+
+    private PostRepository postRepository;
+
+    /**
+     * 게시글 조회
+     * @return
+     */
+    public List<Post> getAllPosts() {
+        return postRepository.findAll();
+    }
+
+    /**
+     * 아이디로 조회
+     * @param id
+     * @return
+     */
+    public Optional<Post> getPostById(Long id) {
+        return postRepository.findById(id);
+    }
+
+    /**
+     * 게시글 생성
+     * @param post
+     * @return
+     */
+    public Post createPost(Post post) {
+        return postRepository.save(post);
+    }
+
+    /**
+     * 게시글 삭제
+     * @param id
+     */
+    public void deletePost(Long id) {
+        postRepository.deleteById(id);
+    }
+}
