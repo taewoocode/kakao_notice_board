f2dec614c9be2151083c6aebc694f67fbb8e5a5e
taewoocode <qkrxodn6035@gmail.com>
2025-02-12 00:19:29 +0900

    카카오 dto 추가

---
+++ src/main/java/com/example/kakao_notice_board/user/controller/KakaoLoginController.java
@@ -0,0 +1,25 @@
+package com.example.kakao_notice_board.user.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class KakaoLoginController {
+
+    @Value("${kakao.client_id}")
+    private String client_id;
+
+    @Value("${kakao.redirect_uri}")
+    private String redirect_uri;
+
+    @GetMapping("/page")
+    public String loginPage(Model model) {
+        String location = "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id="+client_id+"&redirect_uri="+redirect_uri;
+        model.addAttribute("location", location);
+
+        return "login";
+    }
+}
---
+++ src/main/java/com/example/kakao_notice_board/user/dto/KakaoLoginDto.java
@@ -0,0 +1,14 @@
+package com.example.kakao_notice_board.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Builder
+@Getter
+public class KakaoLoginDto {
+
+    public String accessToken;
+    public String refreshToken;
+}
