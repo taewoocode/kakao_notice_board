983ea6fbb36bc4d7e19a38a95f6feecb9252b550
taewoocode <qkrxodn6035@gmail.com>
2025-02-12 00:23:41 +0900

    임포트 패키지 정리

--- src/main/java/com/example/kakao_notice_board/user/controller/KakaoLoginController.java
+++ src/main/java/com/example/kakao_notice_board/user/controller/KakaoLoginController.java
@@ -2,29 +2,14 @@ package com.kakaologin.demo.controller;
 
 import com.example.kakao_notice_board.user.dto.KakaoUserInfoResponseDto;
 import com.example.kakao_notice_board.user.service.KakaoService;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.kakaologin.demo.dto.KakaoUserInfoResponseDto;
-import com.kakaologin.demo.service.KakaoService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
 
 @Slf4j
 @RestController
