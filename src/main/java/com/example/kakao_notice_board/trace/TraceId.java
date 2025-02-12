a21bec892d60af11341a18b8f6fb48cfd62910e8
taewoocode <qkrxodn6035@gmail.com>
2025-02-13 00:53:39 +0900

    로그추적시 생성

---
+++ src/main/java/com/example/kakao_notice_board/trace/TraceId.java
@@ -0,0 +1,44 @@
+package com.example.kakao_notice_board.trace;
+
+import java.util.UUID;
+
+public class TraceId {
+
+    private String id;
+    private int level;
+
+    public TraceId(String id, int level) {
+        this.id = createId();
+        this.level = 0;
+    }
+
+    /**
+     * createId
+     * @return
+     */
+    private String createId() {
+        return UUID.randomUUID().toString().substring(08);
+    }
+
+    /**
+     * createNextTaceId
+     * @return
+     */
+    public TraceId nextTraceId() {
+        return new TraceId(id, level + 1);
+    }
+
+    public boolean isFirstLevel() {
+        return level == 0;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+
+}
