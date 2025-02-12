57c672e4f91ddd682d913e4f7e541dd6dae6bd44
taewoocode <qkrxodn6035@gmail.com>
2025-02-13 00:55:23 +0900

    로그 상태 클래스 생성

---
+++ src/main/java/com/example/kakao_notice_board/trace/TraceStatus.java
@@ -0,0 +1,20 @@
+package com.example.kakao_notice_board.trace;
+
+import lombok.Getter;
+
+@Getter
+public class TraceStatus {
+
+    private TraceId traceId;
+
+    private Long startTimeMs;
+
+    private String message;
+
+    public TraceStatus(TraceId traceId, Long startTimeMs, String message) {
+        this.traceId = traceId;
+        this.startTimeMs = startTimeMs;
+        this.message = message;
+    }
+
+}
